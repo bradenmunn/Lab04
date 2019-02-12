dba463a62ea332c306171a9dfa82d9a2988a6dd9
bradenmunn <bradenmunn@yahoo.com>
2019-02-11 20:58:29 -0600

    Updated the todo list

--- src/Todo.java
+++ src/Todo.java
@@ -7,12 +7,25 @@ OU EMAIL____________: bradenmunn@ou.edu
 Tasks:
 1. Implement the equals() method in the Animal class
 2. Test the new equals() method in the Animal class
+3. Test the Animal toString()
+4. Write tests for Equipment methods
+5. Write tests for the Zoo class getters
+6. Write tests for the Zoo class toString()
 
 Estimated Time (minutes):
 1. 10
 2. 5
+3. 7
+4. 15
+5. 15
+6. 10
 
 Actual Time (minutes):
 1. 6
-2. XXX
+2. 10
+3. 6
+4. 20
+5. 10
+6. 10
+
 */}

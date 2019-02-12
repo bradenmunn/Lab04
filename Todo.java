9477089e6416875c3a691809528b9b432ef175e1
bradenmunn <bradenmunn@yahoo.com>
2019-02-11 20:29:46 -0600

    Updated todo tracker

---
+++ Todo.java
@@ -0,0 +1,24 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-02-07
+OU ID_______________: 113483760
+OU EMAIL____________: bradenmunn@ou.edu
+
+Tasks:
+1. Implement the equals() method in the Animal class
+2. Test the new equals() method in the Animal class
+3. Test the Animal toString()
+4. Write tests for Equipment methods
+
+Estimated Time (minutes):
+1. 10
+2. 5
+3. 7
+4. 15
+
+Actual Time (minutes):
+1. 6
+2. 10
+3. 6
+4. 20
+*/}
